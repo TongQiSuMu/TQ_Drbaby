a3e61a7ca9b516f3d9e79233efd2a57d374167c1
kpl <lh17671881389@163.com>
2025-07-21 10:49:58 +0800

    地址修改

--- src/renderer/utils/config.js
+++ src/renderer/utils/config.js
@@ -6,7 +6,7 @@ const config = {
   // BASE_URL: 'http://192.168.8.38:8089', 
   WSS_URL: 'wss://172.16.6.11:10096',
   // DIFT_URL: 'http://172.16.6.11',
-  DIFT_URL: 'http://192.168.8.37:8012',
+  DIFT_URL: 'http://localhost:8012',
   // 请求超时时间
   REQUEST_TIMEOUT: 10000,
   
